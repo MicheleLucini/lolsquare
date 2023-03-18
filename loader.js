c731bd7d8559bace1a5ed3b1c174364e28c6079a
Michele Lucini <mic.lucini@gmail.com>
2023-03-18 12:16:09 +0100

    test

--- loader.js
+++ loader.js
@@ -2,22 +2,22 @@
 $(function () {
     $.ajax({
         dataType: "text",
-        url: (window.location.href + '/version.txt').replace("//", "/"),
+        url: window.location.href + 'version.txt',
         cache: false,
         success: function (data) {
-            // Bundle css - <link rel="stylesheet" href="https://static.lolsquare.com/lolsquare.css" />
+            // Bundle css - <link rel="stylesheet" href="lolsquare.css" />
             var link = document.createElement("link");
             link.type = "text/css";
             link.rel = "stylesheet";
-            link.href = 'https://static.lolsquare.com/lolsquare.css?' + data;
+            link.href = 'lolsquare.css?' + data;
             document.head.appendChild(link);
 
-            // Bundle js - <script defer="defer" src="https://static.lolsquare.com/lolsquare.js"></script>
+            // Bundle js - <script defer="defer" src="lolsquare.js"></script>
             var script = document.createElement('script');
             script.type = 'text/javascript';
             script.defer = true;
-            script.src = 'https://static.lolsquare.com/lolsquare.js?' + data;
-            link.crossOrigin = "anonymous";
+            script.src = 'lolsquare.js?' + data;
+            // link.crossOrigin = "anonymous";
             document.head.appendChild(script);
         }
     });
